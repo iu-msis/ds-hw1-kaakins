b05ec6f6247eaea7c1965b17b36143656c35ff78
kaakins <kaakins@indiana.edu>
2018-10-08 22:18:39 -0400

    Reverted $arr type back

--- app/models/Comment.php
+++ app/models/Comment.php
@@ -40,12 +40,11 @@ class Comment
     $success = $statement->execute();
 
     // 4. Handle the results
-    $arr = ();
+    $arr;
     while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
       $theComment =  new Comment($row);
       array_push($arr, $theComment);
     }
-
     return $arr;
   }
   public static function getCommentById(int $id) {
