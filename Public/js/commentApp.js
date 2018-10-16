ddf73c884547f700778c9400ecfeaf30b3c078e6
kaakins <kaakins@indiana.edu>
2018-10-15 21:57:18 -0400

    Added POST endpoint

--- Public/js/commentApp.js
+++ Public/js/commentApp.js
@@ -22,6 +22,25 @@ var commentApp = new Vue({
         console.log(err);
       })
     },
+    postComments(){
+      fetch('http://ec2-18-236-123-251.us-west-2.compute.amazonaws.com/api/comment.php', {
+        method:"POST",
+        headers: {
+          'Content-type':'application/json'
+        },
+        body: JSON.stringify({
+          comment:document.getElementById('comment').value
+        })
+      })
+      .then( response => response.json())
+      .then(function(resp) {
+        console.log(resp)
+      })
+      .catch( err => {
+        console.log('TASK FETCH ERROR:');
+        console.log(err);
+      })
+    }
 },
 created (){
   this.fetchComments()
